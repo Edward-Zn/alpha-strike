69fa97b1350a2eaf64176f67cbd2bf703c4d367f
Edward-Zn <edward.zoravarian@gmail.com>
2016-09-21 12:44:43 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<style>
+    .edward {
+        position: relative;
+        text-align: center;
+        text-transform: uppercase;
+        color: darkblue;
+        text-shadow: 2px 1px darkgray;
+        padding-top: 25px;
+    }
+</style>
+<?= '<h1 class="edward">Nothing here yet</h1>'; ?>
