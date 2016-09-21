04c251f49411ded78b424c450f6fa598c872c134
Edward-Zn <edward.zoravarian@gmail.com>
2016-09-21 12:43:52 +0300

    edward.php

---
+++ edward.php
@@ -0,0 +1,11 @@
+<style>
+    .edward {
+        position: relative;
+        text-align: center;
+        text-transform: uppercase;
+        color: darkblue;
+        text-shadow: 2px 1px darkgray;
+        padding-top: 25px;
+    }
+</style>
+<?= '<h1 class="edward">Hello This Is Edward</h1>'; ?>
